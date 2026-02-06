bd2f6a8cafb247648112ab308e37c87f27ba6d4b
Shane Boyce <smcgb1844@gmail.com>
2026-02-05 21:09:44 -0800

    Chore: make import statements consistent

--- call_functions.py
+++ call_functions.py
@@ -1,10 +1,10 @@
 from google import genai
-from google.genai.types import FunctionCallOrDict
+from google.genai.types import FunctionCall
 
-from functions.get_file_content import schema_get_file_content
-from functions.get_files_info import schema_get_files_info
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import schema_write_file, write_file
 
 available_functions = genai.types.Tool(
     function_declarations=[
@@ -15,40 +15,44 @@ available_functions = genai.types.Tool(
     ],
 )
 
+function_map = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "write_file": write_file,
+    "run_python_file": run_python_file,
+}
+
 
 def call_function(function_call: genai.FunctionCall, verbose=False):
 
     if verbose:
-        print(f"Calling function: {function_call.name}({function_call.args})")
-
-    function_map = {
-        "get_files_info": get_files_info,
-        "get_file_content": get_file_content,
-        "write_file": write_file,
-        "run_python_file": run_python_file,
-    }
+        print(f" - Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_call.name}")
 
     function = function_call.name or ""
+
     if function not in function_map:
-        return types.Content(
+        return genai.types.Content(
             role="tool",
             parts=[
-                types.Part.from_function_response(
-                    name=function_name,
-                    response={"error": f"Unknown function: {function_name}"},
+                genai.types.Part.from_function_response(
+                    name=function,
+                    response={"error": f"Unknown function: {function}"},
                 )
             ],
         )
 
     args = dict(function_call.args) if function_call.args else {}
+    args["working_directory"] = "./calculator"
 
     function_result = function_map[function](**args)
     return genai.types.Content(
         role="tool",
         parts=[
-            types.Part.from_function_response(
+            genai.types.Part.from_function_response(
                 name=function,
-                response=function_result,
+                response={"result": function_result},
             )
         ],
     )
